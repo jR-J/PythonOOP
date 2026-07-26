249f3b19bcf894bfe94e0b8eb83c71e9b6c3a47a
jR-J <mcbethjordanalvin@gmail.com>
2026-07-26 14:08:58 +0300

    small script revision

---
+++ checkage.py
@@ -0,0 +1,8 @@
+def is_adult():
+    age = int(input("Enter age: "))
+    if age >= 18:
+        print(f"{age} this is an adult adult") 
+    else:
+        print(f"{age} not an adult")
+
+is_adult()
